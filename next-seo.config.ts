375c868211762eb10363f31fbfca347e0f93df72
thamudi <tamim.hamoudi@gmail.com>
2023-01-13 22:10:58 +0300

    config: add next SEO

---
+++ next-seo.config.ts
@@ -0,0 +1,19 @@
+import { DefaultSeoProps } from 'next-seo';
+
+const config: DefaultSeoProps={
+  title: 'Khalid Komics',
+  description: '',
+  openGraph: {
+    type: 'website',
+    locale: 'en_IE',
+    url: 'https://www.khalidkomic.com',
+    site_name: 'Khalid Komics',
+  },
+  twitter: {
+    handle: '',
+    site: '',
+    cardType: '',
+  }
+}
+
+export default config;
