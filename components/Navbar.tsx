680efe694e5798db41a4ac7c62d393d8763e8ca7
thamudi <tamim.hamoudi@gmail.com>
2023-01-18 11:22:31 +0300

    update: nav bar items to images

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,18 +5,26 @@ export default function Navbar() {
   return (
     <header>
       <div>
-        <a href="index.html">
-          <Image className="logo" width={500} height={500} src="/img/logo.png" alt="" />
-        </a>
+        <Link href="/">
+          <Image className="logo" width={500} height={500} src="/img/site_icons_logo_full.svg" alt="" />
+        </Link>
       </div>
 
       <nav id="nav">
-        <Link href="/">HOME</Link> |
-        <Link href="/comics">COMICS</Link> |
-        <Link href="/archive">ARCHIVE</Link> |
-        <Link href="/about">ABOUT</Link> |
-        <Link href="https://khalidkomics.secure-decoration.com/">Store</Link> |
-        <Link href="about.html">عربي</Link>
+        <Link href="/comics">
+          <Image width={100} height={100} src="/img/nav/icons_comics_en.svg" alt="comics" />
+        </Link> |
+        <Link href="/archive">
+          <Image width={100} height={100} src="/img/nav/icons_comics_en.svg" alt="archive comics" />
+        </Link> |
+        <Link href="/about"><Image width={100} height={100} src="/img/nav/icons_about_en.svg" alt="about khalid komics" /></Link> |
+        <Link href="/contact"><Image width={100} height={100} src="/img/nav/icons_contact_en.svg" alt="contact khalid komics" /></Link> |
+        <Link href="https://khalidkomics.secure-decoration.com/">
+          <Image width={100} height={100} src="/img/nav/icons_store_en.svg" alt="khalid komics store" />
+        </Link> |
+        <Link href="about.html">
+          <Image width={50} height={50} src="/img/icons/icons_lang_ar.svg" alt="switch ar" />
+        </Link>
       </nav>
     </header>
   )
