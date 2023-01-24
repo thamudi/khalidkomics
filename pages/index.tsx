e3cc16618105da65a470a973a90e6bab644c4051
thamudi <tamim.hamoudi@gmail.com>
2023-01-24 08:47:17 +0300

    patch: route issue in the index.tsx
    route the file to comics page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,1 +1,10 @@
-export default function Home() {}
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+
+export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('/comics');
+  }, []);
+}
