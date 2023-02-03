f6da50251723a03d6d32ec38450f3a144cac1ee7
thamudi <tamim.hamoudi@gmail.com>
2023-02-03 20:50:53 +0300

    patch: display more than one comic and some style fixes

--- components/Comic.tsx
+++ components/Comic.tsx
@@ -10,15 +10,18 @@ const Comic = ({ comicData, fetchComic }: any) => {
     ? comicData.data[0].attributes
     : comicData.attributes;
   const comicMeta = comicData.meta?.pagination;
-  const image = getStrapiMedia({ data: comic.comic.data[0] });
-
+  const images = comic.comic.data.map((comicImage: any) => {
+    return getStrapiMedia({ data: comicImage });
+  });
   return (
     <div className="flex flex-col items-center">
       <h1 className="pb-2">{comic.title}</h1>
       {comicMeta && (
         <ComicNav comicMetaData={comicMeta} fetchComic={fetchComic} />
       )}
-      <Media media={image} title={comic.title} width={500} height={600} />
+      {images.map((image: string) => (
+        <Media media={image} title={comic.title} width={500} height={600} />
+      ))}
       {comic.authorsNote?.length && (
         <ComicFootnote authorsNote={comic.authorsNote} />
       )}
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,15 +4,16 @@ import { useEffect, useRef, useState } from 'react';
 
 export default function Navbar() {
   const [scroll, setScroll] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
   const itemNode: any = useRef(null);
 
-  // useEffect(() => {
-  //   window.addEventListener('scroll', () => {
-  //     //TODO: make this transition smoother
-  //     setScroll(window.pageYOffset > itemNode.current.offsetTop);
-  //   });
-  // }, []);
+  useEffect(() => {
+    window.addEventListener('resize', (e) => {
+      console.log(e);
 
+      setIsMobile(false);
+    });
+  }, []);
   return (
     <header>
       <div>
@@ -27,53 +28,62 @@ export default function Navbar() {
           />
         </Link>
       </div>
-
-      <nav id="nav" ref={itemNode} className={scroll ? 'fixed top-0' : ''}>
-        <Link href="/archive">
-          <Image
-            width={90}
-            height={90}
-            src="/img/nav/icons_archive_en.svg"
-            alt="archive comics"
-          />
-        </Link>{' '}
-        |
-        <Link href="/about">
-          <Image
-            width={90}
-            height={90}
-            src="/img/nav/icons_about_en.svg"
-            alt="about khalid komics"
-          />
-        </Link>{' '}
-        |
-        <Link href="/contact">
-          <Image
-            width={90}
-            height={90}
-            src="/img/nav/icons_contact_en.svg"
-            alt="contact khalid komics"
-          />
-        </Link>{' '}
-        |
-        <Link href="https://khalidkomics.secure-decoration.com/">
-          <Image
-            width={90}
-            height={90}
-            src="/img/nav/icons_store_en.svg"
-            alt="khalid komics store"
-          />
-        </Link>{' '}
-        |
-        <Link href="about.html">
-          <Image
-            width={50}
-            height={50}
-            src="/img/icons/icons_lang_ar_2.svg"
-            alt="switch ar"
-          />
-        </Link>
-      </nav>
+      {isMobile ? <MobileNav /> : <DesktopNav />}
     </header>
   );
 }
+
+const DesktopNav = () => {
+  return (
+    <nav id="nav">
+      <Link href="/archive">
+        <Image
+          width={90}
+          height={90}
+          src="/img/nav/icons_archive_en.svg"
+          alt="archive comics"
+        />
+      </Link>{' '}
+      |
+      <Link href="/about">
+        <Image
+          width={90}
+          height={90}
+          src="/img/nav/icons_about_en.svg"
+          alt="about khalid komics"
+        />
+      </Link>{' '}
+      |
+      <Link href="/contact">
+        <Image
+          width={90}
+          height={90}
+          src="/img/nav/icons_contact_en.svg"
+          alt="contact khalid komics"
+        />
+      </Link>{' '}
+      |
+      <Link href="https://khalidkomics.secure-decoration.com/">
+        <Image
+          width={90}
+          height={90}
+          src="/img/nav/icons_store_en.svg"
+          alt="khalid komics store"
+        />
+      </Link>{' '}
+      |
+      <Link href="about.html">
+        <Image
+          width={50}
+          height={50}
+          src="/img/icons/icons_lang_ar_2.svg"
+          alt="switch ar"
+        />
+      </Link>
+    </nav>
+  );
+};
+
+const MobileNav = () => {
+  return <Image width={90} height={90} src="/img/nav/burger.svg" alt="" />;
+};
