158dd1ad0e59e84c121cce74cfe8c35ba1e69032
thamudi <tamim.hamoudi@gmail.com>
2023-02-10 22:54:24 +0300

    refactor: Media component
    add new type of image: overlay

--- components/Media.tsx
+++ components/Media.tsx
@@ -2,9 +2,19 @@ import { getStrapiMedia } from '@/lib/media';
 import Image from 'next/image';
 import { useRef } from 'react';
 
-const Media = ({ media, title, width, height, className }: any) => {
-  const mediaUrl = media.data ? getStrapiMedia(media) : media;
-  const imageFormats = ['jpg', 'png', 'gif', 'jpeg'];
+const Media = ({
+  media,
+  title,
+  width,
+  height,
+  className,
+  subThumbnail,
+}: any) => {
+  const mediaUrl = media?.data ? getStrapiMedia(media) : media;
+  const thumbnailBackground = subThumbnail?.data
+    ? getStrapiMedia(subThumbnail)
+    : subThumbnail;
+  const imageFormats = ['jpg', 'png', 'gif', 'jpeg', 'svg'];
   const mediaFormat = mediaUrl.split('.')[mediaUrl.split('.').length - 1];
 
   const refVideo: any = useRef(null);
@@ -14,13 +24,32 @@ const Media = ({ media, title, width, height, className }: any) => {
     refVideo.current.muted = !refVideo.current.muted;
   };
   return imageFormats.includes(mediaFormat) ? (
-    <Image
-      src={mediaUrl}
-      alt={title}
-      width={width}
-      height={height}
-      className={className}
-    />
+    thumbnailBackground ? (
+      <div className="relative">
+        <Image
+          src={mediaUrl}
+          alt={title}
+          width={width}
+          height={height}
+          className={`${className} absolute opacity-90`}
+        />
+        <Image
+          src={thumbnailBackground}
+          alt={title}
+          width={width}
+          height={height}
+          className={`${className} round`}
+        />
+      </div>
+    ) : (
+      <Image
+        src={mediaUrl}
+        alt={title}
+        width={width}
+        height={height}
+        className={className}
+      />
+    )
   ) : (
     <video
       ref={refVideo}
