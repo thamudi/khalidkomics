c19d89504043ee5d174ae2c3ef39bf13dc5a40f8
Tamim Hamoudi <18720062+thamudi@users.noreply.github.com>
2023-03-15 16:47:31 +0300

    feat: add 404 500 pages (#9)

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -9,44 +9,6 @@ type LayoutProps = {
 };
 
 export default function Layout({ children }: LayoutProps) {
-  const items = [
-    {
-      src: '/img/nav/icons_archive_en.svg',
-      alt: 'archive comics',
-      link: '/archive',
-      text: 'archive',
-      component: false,
-    },
-    {
-      src: '/img/nav/icons_about_en.svg',
-      alt: 'about khalid komics',
-      link: '/about',
-      text: 'about',
-      component: false,
-    },
-    {
-      src: '/img/nav/icons_contact_en.svg',
-      alt: 'contact khalid komics',
-      link: '/contact',
-      text: 'contact',
-      component: false,
-    },
-    {
-      src: '/img/nav/icons_store_en.svg',
-      alt: 'khalid komics store',
-      link: 'https://khalidkomics.secure-decoration.com/',
-      text: 'store',
-      component: false,
-    },
-    //TODO: move this into the navigation bar component
-    // {
-    //   src: '/img/icons/icons_lang_ar_2.svg',
-    //   alt: 'switch ar',
-    //   link: '#',
-    //   component: true,
-    // },
-  ];
-
   return (
     <>
       <Head>
@@ -66,7 +28,7 @@ export default function Layout({ children }: LayoutProps) {
         <meta name="robots" content="index, follow" />
       </Head>
       <header>
-        <Navbar items={items} />
+        <Navbar />
       </header>
       <main
         className="mt-4"
@@ -75,7 +37,7 @@ export default function Layout({ children }: LayoutProps) {
         {children}
       </main>
       <footer>
-        <Footer items={items} />
+        <Footer />
       </footer>
     </>
   );
