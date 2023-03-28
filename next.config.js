02db5be311602689d744dce322295d9ac56674eb
thamudi <tamim.hamoudi@gmail.com>
2023-03-28 09:33:27 +0300

    feat: add next-i18next
    add next-i18next config and setup with basic common transaltions

--- next.config.js
+++ next.config.js
@@ -1,13 +1,12 @@
+const { i18n } = require('./next-i18next.config')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  i18n: {
-    locales: ['en', 'ar'],
-    defaultLocale: 'en'
-  },
+  i18n,
   images: {
     loader: "default",
-    domains: ["localhost", "192.168.1.49"],
+    domains: [process.env.DOMAINS],
   },
 }
 
