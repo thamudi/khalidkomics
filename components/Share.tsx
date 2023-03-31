dd06a0d72de88ca9532fa7fa3da6152a9ed7d246
thamudi <tamim.hamoudi@gmail.com>
2023-03-31 22:31:18 +0300

    refactor: remove unused imports

--- components/Share.tsx
+++ components/Share.tsx
@@ -1,6 +1,5 @@
 import Image from 'next/image';
 import { useRouter } from 'next/router';
-import React, { useEffect, useState } from 'react';
 import copy from 'copy-to-clipboard';
 
 const Share = ({ comicId }: any) => {
