e2e9a8bd11c843669732521dd558fa0efd4e2146
thamudi <tamim.hamoudi@gmail.com>
2023-04-01 10:16:16 +0300

    fix: media with no https
    add a regex checker to check if there is an https request with the media item

--- lib/media.ts
+++ lib/media.ts
@@ -3,7 +3,16 @@ import { getStrapiURL } from './api';
 export function getStrapiMedia(media: any) {
   try {
     const { url } = media.data.attributes ? media.data.attributes : media.data;
-    const imageUrl = url.startsWith('/') ? getStrapiURL(url) : url;
+
+    /**
+     * This is a quick hack because sometimes digital ocean doesn't send the https with the url for some reason.
+     * This script below wont affect regular images with proper url structure.
+     */
+
+    const pattern = new RegExp('^(http|https)://', 'i');
+    let imageUrl = url.startsWith('/') ? getStrapiURL(url) : url;
+    imageUrl = pattern.test(imageUrl) ? imageUrl : `https://${imageUrl}`;
+
     return imageUrl;
   } catch (error) {
     console.error(error);
