d9033d19d7bc41fd9009c0358b388adefc88fe4d
thamudi <tamim.hamoudi@gmail.com>
2023-04-10 18:14:44 +0300

    fix: slider video mute functionality

--- components/Slider.tsx
+++ components/Slider.tsx
@@ -4,10 +4,25 @@ import 'react-awesome-slider/dist/styles.css';
 
 const Slider = ({ images }: any) => {
   const imageFormats = ['jpg', 'png', 'gif', 'jpeg'];
-  const refVideo: any = useRef(null);
-  const unmute = () => {
-    refVideo.current.defaultMuted = !refVideo.current.defaultMuted;
-    refVideo.current.muted = !refVideo.current.muted;
+  const refVideo: any = Array.from({ length: images.length }, (a) =>
+    useRef(null)
+  );
+  //   const items = Array.from({length: 2}, a => useRef(null));
+
+  const unmute = (index: number) => {
+    refVideo[index].current.defaultMuted =
+      !refVideo[index].current.defaultMuted;
+    refVideo[index].current.muted = !refVideo[index].current.muted;
+  };
+  const resetRefVideo = () => {
+    if (refVideo.length) {
+      refVideo.forEach((element: any) => {
+        if (element.current) {
+          element.current.muted = true;
+          element.current.defaultMuted = true;
+        }
+      });
+    }
   };
 
   return (
@@ -15,6 +30,7 @@ const Slider = ({ images }: any) => {
       infinite={false}
       transitionDelay={1}
       className={'slider awssld'}
+      onTransitionRequest={resetRefVideo}
     >
       {images.map((image: string, i: string) => {
         const mediaFormat = image.split('.')[image.split('.').length - 1];
@@ -22,13 +38,14 @@ const Slider = ({ images }: any) => {
           return <div data-src={image} key={i} />;
         } else {
           return (
-            <div>
+            <div key={i}>
               <video
-                ref={refVideo}
+                id={i}
+                ref={refVideo[i]}
                 loop
                 autoPlay
                 muted
-                onClick={unmute}
+                onClick={() => unmute(Number(i))}
                 className="mb-4 cursor-pointer"
                 style={{ width: '500px', height: '500px' }}
                 src={image}
